d4c9510887dba181be99aff563262824bb83a17d
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-08-29 14:51:09 -0700

    parent new left

--- binary_trees.h
+++ binary_trees.h
@@ -51,5 +51,6 @@ typedef struct levelorder_queue_s
 /* Prototypes */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif /*BINARY_TREES_H*/
