b3cf671b14381b0156bad5661863d19dbd23dec5
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-08-29 15:02:29 -0700

    delete tree

--- binary_trees.h
+++ binary_trees.h
@@ -53,5 +53,6 @@ typedef struct levelorder_queue_s
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
 
 #endif /*BINARY_TREES_H*/
